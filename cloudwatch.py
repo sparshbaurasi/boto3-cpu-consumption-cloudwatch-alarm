56385d08cbd3e1756b470656356e544078acc0e6
sparshbaurasi <107546419+sparshbaurasi@users.noreply.github.com>
2022-10-16 16:04:54 +0530

    First commit

--- cloudwatch.py
+++ cloudwatch.py
@@ -1,8 +1,10 @@
 import boto3
-sns_topic_arn = 'Desired sns topic arn'
-notification_email='example@example.com'
+client_cw = boto3.client('cloudwatch')
+sns_topic_arn = 'Desired sns topic arn' # Here you put sns topic arn on which the alarm will will send a notification to when it gets triggered
+notification_email='example@example.com' # Here paste the email on which the sns topic will send notification to
+alarm_name='alarm name that the user wants' 
 alarm= {
-    "Your Instance id" : "Threshold on which the alarm will trigger(Must be an integer value)" ,
+    "i-Random string" : 1 , # Here you put the instance id for which you want to create the alarm and for its value you put at which threshold the alarm will trigger eg: 1,2,...100 
 }
 keys = list(alarm.keys())
 values = list(alarm.values())
@@ -13,10 +15,9 @@ response2 = client_sns.subscribe(
     Endpoint=notification_email,
     ReturnSubscriptionArn=True
 )
-client_cw = boto3.client('cloudwatch')
 for i,j in zip(keys,values):
  response = client_cw.put_metric_alarm(
-          AlarmName='ec2-cpu-alarmsadasd',
+          AlarmName=alarm_name,
           ActionsEnabled=True,
           AlarmActions=[
                sns_topic_arn,
